aa78f97fdc9fad112fcba7fc4be1d17cdf8f205c
Samuel Webb <samuel.webb@cern.ch>
2020-02-24 13:36:19 +0000

    Add rotation back to sector 0

--- rotate.py
+++ rotate.py
@@ -10,12 +10,12 @@ def rotate_cell_CEE(cell):
     rotated = [-cell[1],cell[0]-cell[1]]
     return rotated
 
-def rotate_cell_CEH_Even(cell):
+def rotate_cell_CEH_Odd(cell):
 
     rotated = [-1-cell[1],-1+cell[0]-cell[1]]
     return rotated
 
-def rotate_cell_CEH_Odd(cell):
+def rotate_cell_CEH_Even(cell):
 
     rotated = [1-cell[1],1+cell[0]-cell[1]]
     return rotated
@@ -28,6 +28,7 @@ def rotate(u,v,layer,sector):
     
     if (layer > 28):
         if (layer % 2) == 0:
+
             if ( sector == 1 ):
                 return rotate_cell_CEH_Even(uv)
             elif ( sector == 2 ):
@@ -47,4 +48,38 @@ def rotate(u,v,layer,sector):
             return rotate_cell_CEE(rotate_cell_CEE(uv))
 
             
+def rotate_to_sector_0(u,v,layer):
 
+    uv = [u,v]
+
+    if (layer > 28):
+
+        if (layer % 2) == 0: #Even CEH Layers
+
+            if ( u > 0 and v > 0 ): #sector 0
+                return uv
+            elif ( u <= 0 and v > u ): #sector 1
+                return rotate_cell_CEH_Even(rotate_cell_CEH_Even(uv)) 
+            else: #sector 2
+                return rotate_cell_CEH_Even(uv)
+            
+        else: #Odd CEH Layers
+                
+            if ( u >= 0 and v >= 0 ): #sector 0
+                return uv
+            elif ( u < 0 and v >= u ): #sector 1
+                return rotate_cell_CEH_Odd(rotate_cell_CEH_Odd(uv)) 
+            else: #sector 2
+                return rotate_cell_CEH_Odd(uv)
+
+    else: #CEE
+
+        if ( u > 0 and v >= 0 ): #sector 0
+            return uv
+        elif ( u <= 0 and v > u ): #sector 1
+            return rotate_cell_CEE(rotate_cell_CEE(uv)) 
+        else: #sector 2
+            return rotate_cell_CEE(uv)
+
+
+            
