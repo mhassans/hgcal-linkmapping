7af241b7cf0fc572f84f4a44c199a387780c8a5f
Samuel Webb <samuel.webb@cern.ch>
2020-05-19 12:40:03 +0200

    Fill RoverZHistograms in 2D

--- extract_data.cxx
+++ extract_data.cxx
@@ -175,7 +175,7 @@ int main(){
   // TString flat_file_silicon = "words_all_v11_silicon.txt";
   // TString flat_file_scintillator = "words_all_v11_scintillator.txt";
 
-  TString input_file = "../small_v11_neutrino_gun_200415.root.root";
+  TString input_file = "../small_v11_neutrino_gun_200415.root";
   TString flat_file_silicon = "words_all_v11_silicon_neutrino_gun.txt";
   TString flat_file_scintillator = "words_all_v11_scintillator_neutrino_gun.txt";
 
@@ -191,7 +191,7 @@ int main(){
   TFile * file = new TFile(input_file,"READ");
   TTree * tree = (TTree*)file->Get("HGCalTriggerNtuple");
 
-  bool createFlatFile = true;
+  bool createFlatFile = false;
   
   // Declaration of leaf types
   std::vector<int>     *tc_layer = 0;
@@ -250,18 +250,18 @@ int main(){
 
     //R/Z Histograms
 
-    TH1D * ROverZ_Inclusive = new TH1D("ROverZ_Inclusive","",42,0.076,0.58);
-    TH1D * ROverZ_Inclusive_Phi60 = new TH1D("ROverZ_Inclusive_Phi60","",42,0.076,0.58);
-    std::map<std::tuple<int,int,int,int>,TH1D*> ROverZ_per_module;
-    std::map<std::tuple<int,int,int,int>,TH1D*> ROverZ_per_module_Phi60;
+    TH2D * ROverZ_Inclusive = new TH2D("ROverZ_Inclusive","",42,0.076,0.58,12,0,2*M_PI/3);
+    //TH1D * ROverZ_Inclusive_Phi60 = new TH1D("ROverZ_Inclusive_Phi60","",42,0.076,0.58);
+    std::map<std::tuple<int,int,int,int>,TH2D*> ROverZ_per_module;
+    //std::map<std::tuple<int,int,int,int>,TH1D*> ROverZ_per_module_Phi60;
     //Create one for each module (silicon at first)
     for ( int i = 0; i < 15; i++){//u
       for ( int j = 0; j < 15; j++){//v
 	for ( int k = 1; k < 53; k++){//layer
 
 	  if ( k < 28 && k%2 == 0 ) continue;
-	  ROverZ_per_module[std::make_tuple(0,i,j,k)] = new TH1D( ("ROverZ_silicon_" + std::to_string(i) + "_" +  std::to_string(j) +"_"+ std::to_string(k)).c_str(),"",42,0.076,0.58);
-	  ROverZ_per_module_Phi60[std::make_tuple(0,i,j,k)] = new TH1D( ("ROverZ_Phi60_silicon_" + std::to_string(i) + "_" +  std::to_string(j) +"_"+ std::to_string(k)).c_str(),"",42,0.076,0.58);
+	  ROverZ_per_module[std::make_tuple(0,i,j,k)] = new TH2D( ("ROverZ_silicon_" + std::to_string(i) + "_" +  std::to_string(j) +"_"+ std::to_string(k)).c_str(),"",42,0.076,0.58,12,0,2*M_PI/3);
+	  //ROverZ_per_module_Phi60[std::make_tuple(0,i,j,k)] = new TH1D( ("ROverZ_Phi60_silicon_" + std::to_string(i) + "_" +  std::to_string(j) +"_"+ std::to_string(k)).c_str(),"",42,0.076,0.58);
 	  
 	}
       }
@@ -271,8 +271,8 @@ int main(){
       for ( int j = 0; j < 12; j++){
 	for ( int k = 37; k < 53; k++){
 
-	  ROverZ_per_module[std::make_tuple(1,i,j,k)] = new TH1D( ("ROverZ_scintillator_" + std::to_string(i) + "_" +  std::to_string(j) +"_"+ std::to_string(k)).c_str(),"",42,0.076,0.58);
-	  ROverZ_per_module_Phi60[std::make_tuple(1,i,j,k)] = new TH1D( ("ROverZ_Phi60_scintillator_" + std::to_string(i) + "_" +  std::to_string(j) +"_"+ std::to_string(k)).c_str(),"",42,0.076,0.58);
+	  ROverZ_per_module[std::make_tuple(1,i,j,k)] = new TH2D( ("ROverZ_scintillator_" + std::to_string(i) + "_" +  std::to_string(j) +"_"+ std::to_string(k)).c_str(),"",42,0.076,0.58,12,0,2*M_PI/3);
+	  //ROverZ_per_module_Phi60[std::make_tuple(1,i,j,k)] = new TH1D( ("ROverZ_Phi60_scintillator_" + std::to_string(i) + "_" +  std::to_string(j) +"_"+ std::to_string(k)).c_str(),"",42,0.076,0.58);
 
 	}
       }
@@ -294,7 +294,7 @@ int main(){
     for (Long64_t jentry=0; jentry<nentries;jentry++) {
       nb = tree->GetEntry(jentry);   
       if (jentry % 100 == 0) std::cout << jentry << " / " << nentries << std::endl;;
-      //      if (jentry > 100 )break;
+      //if (jentry > 100 )break;
 
       for (int j = 0;j<tc_waferu->size();j++){
 
@@ -304,9 +304,9 @@ int main(){
 	//Fill Eta Histograms	
 	std::pair<float,float> eta_phi = getROverZPhi(tc_x->at(j),tc_y->at(j),tc_z->at(j));
 	
-	ROverZ_Inclusive->Fill(std::abs(eta_phi.first));
-	if ( eta_phi.second < M_PI/3 )
-	  ROverZ_Inclusive_Phi60->Fill(std::abs(eta_phi.first));
+	ROverZ_Inclusive->Fill(std::abs(eta_phi.first),eta_phi.second);
+	// if ( eta_phi.second < M_PI/3 )
+	//   ROverZ_Inclusive_Phi60->Fill(std::abs(eta_phi.first));
 
 	
 	if ( u > -990 ){//Silicon
@@ -321,9 +321,9 @@ int main(){
 	  }
 
 
-	  ROverZ_per_module[std::make_tuple(0,uv.first,uv.second,tc_layer->at(j))]->Fill(std::abs(eta_phi.first));
-	  if ( eta_phi.second < M_PI/3 )
-	    ROverZ_per_module_Phi60[std::make_tuple(0,uv.first,uv.second,tc_layer->at(j))]->Fill(std::abs(eta_phi.first));
+	  ROverZ_per_module[std::make_tuple(0,uv.first,uv.second,tc_layer->at(j))]->Fill(std::abs(eta_phi.first),eta_phi.second);
+	  // if ( eta_phi.second < M_PI/3 )
+	  //   ROverZ_per_module_Phi60[std::make_tuple(0,uv.first,uv.second,tc_layer->at(j))]->Fill(std::abs(eta_phi.first));
 	      
 	  
 	}
@@ -341,9 +341,9 @@ int main(){
 	  }
 
 
-	  ROverZ_per_module[std::make_tuple(1,etaphi.first,etaphi.second,tc_layer->at(j))]->Fill(std::abs(eta_phi.first));
-	  if ( eta_phi.second < M_PI/3 )
-	    ROverZ_per_module_Phi60[std::make_tuple(1,etaphi.first,etaphi.second,tc_layer->at(j))]->Fill(std::abs(eta_phi.first));
+	  ROverZ_per_module[std::make_tuple(1,etaphi.first,etaphi.second,tc_layer->at(j))]->Fill(std::abs(eta_phi.first),eta_phi.second);
+	  // if ( eta_phi.second < M_PI/3 )
+	  //   ROverZ_per_module_Phi60[std::make_tuple(1,etaphi.first,etaphi.second,tc_layer->at(j))]->Fill(std::abs(eta_phi.first));
 
 
 
@@ -462,14 +462,14 @@ int main(){
     TFile * file_out = new TFile("ROverZHistograms.root","RECREATE");
     file_out->cd();
     ROverZ_Inclusive->Write();
-    ROverZ_Inclusive_Phi60->Write();
+    //    ROverZ_Inclusive_Phi60->Write();
 
     for (auto& x: ROverZ_per_module) {
       x.second->Write();
     }
-    for (auto& x: ROverZ_per_module_Phi60) {
-      x.second->Write();
-    }
+    // for (auto& x: ROverZ_per_module_Phi60) {
+    //   x.second->Write();
+    // }
 
     file_out->Close();
     //out_words->SaveAs("hist.root");
