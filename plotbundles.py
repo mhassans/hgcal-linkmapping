7c26ce5f3bb0e24ed3b34fc36d7dda680006fdc2
Samuel Webb <samuel.webb@cern.ch>
2020-08-19 12:16:28 +0100

    renaming regionA,B to phidivisionX,Y to avoid ambiguity

--- plotbundles.py
+++ plotbundles.py
@@ -82,12 +82,12 @@ def main():
     filein = ROOT.TFile("lpgbt_10.root")
     
     inclusive_hists = []
-    regionA_hists = []
-    regionB_hists = []
+    phidivisionX_hists = []
+    phidivisionY_hists = []
     inclusive_hists_ratio = []
-    inclusive_hists_ratio_to_regionB = []
-    regionA_hists_ratio = []
-    regionB_hists_ratio = []
+    inclusive_hists_ratio_to_phidivisionX = []
+    phidivisionX_hists_ratio = []
+    phidivisionY_hists_ratio = []
 
     #Load config file if exists,
     #Otherwise assume .root file input
@@ -127,19 +127,19 @@ def main():
         data = loadDataFile(MappingFile) #dataframe
         minigroups,minigroups_swap = getMinilpGBTGroups(data)
 
-        #Configuration for how to divide TCs into RegionA and RegionB (traditionally phi > 60 and phi < 60)
+        #Configuration for how to divide TCs into phidivisionX and phidivisionY (traditionally phi > 60 and phi < 60)
         split = "per_roverz_bin"
-        RegionA_fixvalue_min = 55
-        RegionB_fixvalue_max = None
+        phidivisionX_fixvalue_min = 55
+        phidivisionY_fixvalue_max = None
         
         if phisplitConfig != None:
             split = phisplitConfig['type']
-            if 'RegionA_fixvalue_min' in phisplitConfig.keys():
-                RegionA_fixvalue_min = phisplitConfig['RegionA_fixvalue_min']
-            if 'RegionB_fixvalue_max' in phisplitConfig.keys():
-                RegionB_fixvalue_max = phisplitConfig['RegionB_fixvalue_max']
+            if 'phidivisionX_fixvalue_min' in phisplitConfig.keys():
+                phidivisionX_fixvalue_min = phisplitConfig['phidivisionX_fixvalue_min']
+            if 'phidivisionY_fixvalue_max' in phisplitConfig.keys():
+                phidivisionY_fixvalue_max = phisplitConfig['phidivisionY_fixvalue_max']
 
-        inclusive_hists_input,module_hists = getModuleHists(CMSSW_ModuleHists, split = split, RegionA_fixvalue_min = RegionA_fixvalue_min, RegionB_fixvalue_max = RegionB_fixvalue_max)
+        inclusive_hists_input,module_hists = getModuleHists(CMSSW_ModuleHists, split = split, phidivisionX_fixvalue_min = phidivisionX_fixvalue_min, phidivisionY_fixvalue_max = phidivisionY_fixvalue_max)
         if 'corrections' in config.keys():
             if config['corrections'] != None:
                 print ( "Applying geometry corrections" )
@@ -151,33 +151,33 @@ def main():
         bundled_hists = getBundledlpgbtHistsRoot(minigroup_hists_root,bundles)
 
         inclusive = inclusive_hists_input[0].Clone("inclusive_hists_input_inclusive")
-        regionA = inclusive_hists_input[0].Clone("inclusive_hists_input_regionA")
-        regionB = inclusive_hists_input[1]
+        phidivisionX = inclusive_hists_input[0].Clone("inclusive_hists_input_phidivisionX")
+        phidivisionY = inclusive_hists_input[1]
         inclusive.Add( inclusive_hists_input[1] )
         inclusive.Scale(1./24)
-        regionA.Scale(1./24)
-        regionB.Scale(1./24)
+        phidivisionX.Scale(1./24)
+        phidivisionY.Scale(1./24)
 
-        for i,(hist_regionA,hist_regionB) in enumerate(zip(bundled_hists[0].values(),bundled_hists[1].values())):
+        for i,(hist_phidivisionX,hist_phidivisionY) in enumerate(zip(bundled_hists[0].values(),bundled_hists[1].values())):
 
-            inclusive_hist = hist_regionA.Clone("inclusive_hists_input_regionA" + str(i))
+            inclusive_hist = hist_phidivisionX.Clone("inclusive_hists_input_phidivisionX" + str(i))
 
             inclusive_hists.append(inclusive_hist)
-            inclusive_hists[-1].Add( hist_regionB )
+            inclusive_hists[-1].Add( hist_phidivisionY )
             inclusive_hists_ratio.append( inclusive_hists[-1].Clone("inclusive_ratio_" + str(i) ) )
             inclusive_hists_ratio[-1].Divide(inclusive)
 
-            regionA_hists.append(hist_regionA)
-            regionA_hists_ratio.append(hist_regionA.Clone("regionA_ratio_" + str(i) ))
-            regionA_hists_ratio[-1].Divide(regionA)
+            phidivisionX_hists.append(hist_phidivisionX)
+            phidivisionX_hists_ratio.append(hist_phidivisionX.Clone("phidivisionX_ratio_" + str(i) ))
+            phidivisionX_hists_ratio[-1].Divide(phidivisionX)
             
-            regionB_hists.append(hist_regionB)
-            regionB_hists_ratio.append(hist_regionB.Clone("regionB_ratio_" + str(i) ))
-            regionB_hists_ratio[-1].Divide(regionB)
+            phidivisionY_hists.append(hist_phidivisionY)
+            phidivisionY_hists_ratio.append(hist_phidivisionY.Clone("phidivisionY_ratio_" + str(i) ))
+            phidivisionY_hists_ratio[-1].Divide(phidivisionY)
 
-            inclusive_hists_ratio_to_regionB.append(inclusive_hists[-1].Clone("inclusive_ratio_to_regionB_" + str(i) ))
-            inclusive_hists_ratio_to_regionB[-1].Divide(hist_regionB)
-            inclusive_hists_ratio_to_regionB[-1].SetLineColor(1+i)
+            inclusive_hists_ratio_to_phidivisionY.append(inclusive_hists[-1].Clone("inclusive_ratio_to_phidivisionY_" + str(i) ))
+            inclusive_hists_ratio_to_phidivisionY[-1].Divide(hist_phidivisionY)
+            inclusive_hists_ratio_to_phidivisionY[-1].SetLineColor(1+i)
 
         module_hists = None
         inclusive_hists_input = None
@@ -188,39 +188,39 @@ def main():
         bundled_hists = None
 
     elif useROOT:
-        regionA = filein.Get("ROverZ_PhiRegionA")
-        regionB = filein.Get("ROverZ_PhiRegionB")
-        ROOT.TH1.Add(regionA,regionB)
+        phidivisionX = filein.Get("ROverZ_PhiDivisionX")
+        phidivisionY = filein.Get("ROverZ_PhiDivisionY")
+        ROOT.TH1.Add(phidivisionX,phidivisionY)
 
         for i in range (24):
-            regionA_hists.append( filein.Get("lpgbt_ROverZ_bundled_"+str(i)+"_0") )
-            regionB_hists.append( filein.Get("lpgbt_ROverZ_bundled_"+str(i)+"_1") )
-            inclusive_hists.append( regionA_hists[-1].Clone("inclusive_hists_input_regionA" + str(i)) )
+            phidivisionX_hists.append( filein.Get("lpgbt_ROverZ_bundled_"+str(i)+"_0") )
+            phidivisionY_hists.append( filein.Get("lpgbt_ROverZ_bundled_"+str(i)+"_1") )
+            inclusive_hists.append( phidivisionX_hists[-1].Clone("inclusive_hists_input_phidivisionX" + str(i)) )
             
-            ROOT.TH1.Add(inclusive_hists[-1],regionB_hists[-1])
+            ROOT.TH1.Add(inclusive_hists[-1],phidivisionY_hists[-1])
             
             inclusive_hists_ratio.append (  inclusive_hists[-1].Clone ("inclusive_ratio_" + str(i)  )  )
-            regionA_hists_ratio.append (  regionA_hists[-1].Clone ("regionA_ratio_" + str(i)  )  )
-            regionB_hists_ratio.append (  regionB_hists[-1].Clone ("regionB_ratio_" + str(i)  )  )
+            phidivisionX_hists_ratio.append (  phidivisionX_hists[-1].Clone ("phidivisionX_ratio_" + str(i)  )  )
+            phidivisionY_hists_ratio.append (  phidivisionY_hists[-1].Clone ("phidivisionY_ratio_" + str(i)  )  )
             
             
-            inclusive_hists_ratio_to_regionB.append (  inclusive_hists[-1].Clone ("inclusive_ratio_to_regionB_" + str(i)  )  )
+            inclusive_hists_ratio_to_phidivisionY.append (  inclusive_hists[-1].Clone ("inclusive_ratio_to_phidivisionY_" + str(i)  )  )
 
             inclusive_hists_ratio[-1].Divide( inclusive )            
-            regionA_hists_ratio[-1].Divide( regionA  )
-            regionB_hists_ratio[-1].Divide( regionB  )
-            inclusive_hists_ratio_to_regionB[-1].Divide( regionB  )            
+            phidivisionX_hists_ratio[-1].Divide( phidivisionX  )
+            phidivisionY_hists_ratio[-1].Divide( phidivisionY  )
+            inclusive_hists_ratio_to_phidivisionY[-1].Divide( phidivisionY  )            
 
         
     print_ratio_plot(inclusive,inclusive_hists,inclusive_hists_ratio,"inclusive")
-    print_ratio_plot(regionA,regionA_hists,regionA_hists_ratio,"regionA")
-    print_ratio_plot(regionB,regionB_hists,regionB_hists_ratio,"regionB")
+    print_ratio_plot(phidivisionX,phidivisionX_hists,phidivisionX_hists_ratio,"phidivisionX")
+    print_ratio_plot(phidivisionY,phidivisionY_hists,phidivisionY_hists_ratio,"phidivisionY")
 
-    print_ratio_plot(inclusive,inclusive_hists,inclusive_hists_ratio_to_regionB,"inclusive_to_regionB")
+    print_ratio_plot(inclusive,inclusive_hists,inclusive_hists_ratio_to_phidivisionY,"inclusive_to_phidivisionY")
 
     print_numpy_plot( inclusive_hists, "numpy_inclusive")
-    print_numpy_plot( regionA_hists, "numpy_regionA")
-    print_numpy_plot( regionB_hists, "numpy_regionB")
+    print_numpy_plot( phidivisionX_hists, "numpy_phidivisionX")
+    print_numpy_plot( phidivisionY_hists, "numpy_phidivisionY")
 
 main()
     
